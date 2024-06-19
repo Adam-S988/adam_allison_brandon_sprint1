977e4eae78fe19ede18a3f7b0e210bee7b26fe62
Adam-S988 <adam.stevenson@keyin.com>
2024-06-19 13:57:32 -0230

    next commit

--- Script/MenuScript.js
+++ Script/MenuScript.js
@@ -1,12 +1,15 @@
 const roundBoxes = document.querySelectorAll(".roundBoxes");
+const backgroundGrayBox = document.querySelector(".backgroundGrayBox");
 
 roundBoxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (box.classList.contains("active")) {
       box.classList.remove("active");
+      backgroundGrayBox.style.height = "500px";
     } else {
       removeActiveClasses();
       box.classList.add("active");
+      backgroundGrayBox.style.height = "750px";
     }
   });
 });
