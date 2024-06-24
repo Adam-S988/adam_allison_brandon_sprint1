9631ac02e0182a6ef4fd36df622075950b77f4eb
Adam-S988 <adam.stevenson@keyin.com>
2024-06-24 10:55:16 -0230

    Media Queries

--- Script/MenuScript.js
+++ Script/MenuScript.js
@@ -1,16 +1,41 @@
 const roundBoxes = document.querySelectorAll(".roundBoxes");
 const backgroundGrayBox = document.querySelector(".backgroundGrayBox");
 
+function updateBackgroundGrayBoxHeight() {
+  const anyActive = Array.from(roundBoxes).some((box) =>
+    box.classList.contains("active")
+  );
+  const windowWidth = window.innerWidth;
+
+  if (anyActive) {
+    if (windowWidth <= 700) {
+      backgroundGrayBox.style.height = "3050px";
+    } else if (windowWidth <= 1000) {
+      backgroundGrayBox.style.height = "2050px";
+    } else {
+      backgroundGrayBox.style.height = "1640px";
+    }
+  } else {
+    if (windowWidth <= 700) {
+      backgroundGrayBox.style.height = "2800px";
+    } else if (windowWidth <= 1000) {
+      backgroundGrayBox.style.height = "1800px";
+    } else {
+      backgroundGrayBox.style.height = "1380px";
+    }
+  }
+}
+
 roundBoxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (box.classList.contains("active")) {
       box.classList.remove("active");
-      backgroundGrayBox.style.height = "1300px";
     } else {
       removeActiveClasses();
       box.classList.add("active");
-      backgroundGrayBox.style.height = "1570px";
     }
+
+    updateBackgroundGrayBoxHeight();
   });
 });
 
@@ -19,3 +44,7 @@ function removeActiveClasses() {
     box.classList.remove("active");
   });
 }
+
+updateBackgroundGrayBoxHeight();
+
+window.addEventListener("resize", updateBackgroundGrayBoxHeight);
