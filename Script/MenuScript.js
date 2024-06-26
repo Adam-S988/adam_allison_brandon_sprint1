62ab366f71236fa112139e7ec3bd7e1b4d5f0d01
Adam-S988 <adam.stevenson@keyin.com>
2024-06-26 08:28:31 -0230

    First Tuesday Commit

--- Script/MenuScript.js
+++ Script/MenuScript.js
@@ -6,24 +6,6 @@ function updateBackgroundGrayBoxHeight() {
     box.classList.contains("active")
   );
   const windowWidth = window.innerWidth;
-
-  // if (anyActive) {
-  //   if (windowWidth <= 700) {
-  //     backgroundGrayBox.style.height = "3050px";
-  //   } else if (windowWidth <= 1100) {
-  //     backgroundGrayBox.style.height = "2050px";
-  //   } else {
-  //     backgroundGrayBox.style.height = "1640px";
-  //   }
-  // } else {
-  //   if (windowWidth <= 700) {
-  //     backgroundGrayBox.style.height = "2800px";
-  //   } else if (windowWidth <= 1100) {
-  //     backgroundGrayBox.style.height = "1800px";
-  //   } else {
-  //     backgroundGrayBox.style.height = "1380px";
-  //   }
-  // }
 }
 
 roundBoxes.forEach((box) => {
